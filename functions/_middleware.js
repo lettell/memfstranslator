df361bf22184f2527e5404698c1f037083bf7431
Paulius <jarosius@live.com>
2024-04-14 18:06:44 +0300

    try refactored

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -31,6 +31,7 @@
 //     // If not a proxied path, handle the request normally
 //     return fetch(request)
 // }
+const originalAPIHostname = "github.com";
 
 export async function onRequest(context) {
     const {
@@ -45,38 +46,65 @@ export async function onRequest(context) {
     const url = new URL(request.url)
 
     if (url.pathname.startsWith('/lettell')) {
-        const url_hostname = url.hostname;
-        const githubUrl = 'https://github.com' + url.pathname
-        const method = request.method;
-        const request_headers = request.headers;
-        const new_request_headers = new Headers(request_headers);
+        const url = new URL(request.url);
+        url.hostname = originalAPIHostname;
 
-        new_request_headers.set('Host', 'github.com');
-        new_request_headers.set('Referer', url.protocol + '//' + url_hostname);
+        // If your API is located on api.mysite.com/anyname (without "api/" in the path),
+        // remove the "api/" part of example.com/api/name
+   
+        // url.pathname = url.pathname.substring(4)
+   
+        // Best practice is to always use the original request to construct the new request
+        // to clone all the attributes. Applying the URL also requires a constructor
+        // since once a Request has been constructed, its URL is immutable.
+        const newRequest = new Request(url.toString(), request);
+        try {
+            const response = await fetch(newRequest);
+     
+            // Copy over the response
+            const modifiedResponse = new Response(response.body, response);
+     
+            // Delete the set-cookie from the response so it doesn't override existing cookies
+            modifiedResponse.headers.delete("set-cookie");
+     
+            return modifiedResponse;
+          } catch (e) {
+            return new Response(JSON.stringify({ error: e.message }), {
+              status: 500,
+            });
+          }   
+        // const url_hostname = url.hostname;
+        // const githubUrl = 'https://github.com' + url.pathname
+        // const method = request.method;
+        // const request_headers = request.headers;
+        // const new_request_headers = new Headers(request_headers);
 
-        let original_response = await fetch(githubUrl, {
-            method: method,
-            headers: new_request_headers
-        })
+        // new_request_headers.set('Host', 'github.com');
+        // new_request_headers.set('Referer', url.protocol + '//' + url_hostname);
 
-        let original_response_clone = original_response.clone();
-        let response_headers = original_response.headers;
-        let new_response_headers = new Headers(response_headers);
-        let status = original_response.status;
+        // let original_response = await fetch(githubUrl, {
+        //     method: method,
+        //     headers: new_request_headers
+        // })
 
-        new_response_headers.set('access-control-allow-origin', '*');
-        new_response_headers.set('access-control-allow-credentials', true);
-        new_response_headers.delete('content-security-policy');
-        new_response_headers.delete('content-security-policy-report-only');
-        new_response_headers.delete('clear-site-data');
+        // let original_response_clone = original_response.clone();
+        // let response_headers = original_response.headers;
+        // let new_response_headers = new Headers(response_headers);
+        // let status = original_response.status;
+
+        // new_response_headers.set('access-control-allow-origin', '*');
+        // new_response_headers.set('access-control-allow-credentials', true);
+        // new_response_headers.delete('content-security-policy');
+        // new_response_headers.delete('content-security-policy-report-only');
+        // new_response_headers.delete('clear-site-data');
 
 
 
 
-        return new Response(original_response_clone.body, {
-            status,
-            headers: new_response_headers
-        })
+        // return new Response(original_response_clone.body, {
+        //     status,
+        //     headers: new_response_headers
+        // })
     } else {
         return await next()
 
