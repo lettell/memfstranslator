784bffddf7f1216c92eee7434189d31c3559f601
Paulius <jarosius@live.com>
2024-04-14 20:04:32 +0300

    add context

--- src/base/BaseTabs.tsx
+++ src/base/BaseTabs.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { TabList, Tab } from '@fluentui/react-components';
+import { useRecoilState } from 'recoil';
+import { selectedContextState } from 'features/auth/state';
 
 const GitBasedTabs = ({ tabs }: any) => {
-    const [selectedValue, setSelectedValue] = React.useState<any>('conditions');
+    // TODO: move to attom
 
+    // const [selectedValue, setSelectedValue] = React.useState<any>('conditions');
+    const [selectedValue, setSelectedValue] = useRecoilState(selectedContextState);
+    
     const onTabSelect = (data: any) => {
         console.log(`The ${data.value} tab was selected`);
         setSelectedValue(data.value);
--- src/features/auth/state.ts
+++ src/features/auth/state.ts
@@ -1,10 +1,15 @@
 import { atom } from "recoil";
 
+// DEMO globals
 const isAuthState = atom({
     key: '@authState',
     default: false,
 })
-
+const selectedContextState = atom({
+    key: '@selectedContext',
+    default: null,
+})
 export {
-    isAuthState
+    isAuthState,
+    selectedContextState
 }
